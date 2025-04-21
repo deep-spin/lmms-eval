15377a9dba826677fd976974981db8fa40ef35f5
aminfarajian <143111778+aminfarajian@users.noreply.github.com>
2025-04-21 17:30:24 +0100

    Update llava.py
    Rollback to the default system prompt for llava.

--- lmms_eval/models/llava.py
+++ lmms_eval/models/llava.py
@@ -65,7 +65,7 @@ class Llava(lmms):
         tie_weights: bool = True,
         truncate_context=False,  # whether to truncate the context in generation, set it False for LLaVA-1.6
         customized_config=None,  # ends in json
-        add_system_prompt="<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
+        add_system_prompt=None,
         **kwargs,
     ) -> None:
         super().__init__()
@@ -143,7 +143,7 @@ class Llava(lmms):
             self.model.to(self._device)
             self._rank = 0
             self._world_size = 1
-        self.system_prompt = add_system_prompt
+        self.add_system_prompt = add_system_prompt
     @property
     def config(self):
         # return the associated transformers.AutoConfig for the given pretrained model.
@@ -373,7 +373,7 @@ class Llava(lmms):
                     conv = conv_templates[self.conv_template].copy()
                 # if conv.system:
                 #     conv.system = self.add_system_prompt
-                if self.system_prompt and conv.system:
+                if self.add_system_prompt and conv.system:
                     conv.system = self.system_prompt
                 conv.append_message(conv.roles[0], question)
                 conv.append_message(conv.roles[1], None)
