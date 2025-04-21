16a1908134559fb9e268de088f2782036f06cb37
farajian <amin@unbabel.com>
2025-04-21 17:27:23 +0000

    small fixes in judge_utils.py

--- lmms_eval/tasks/ayavisionbench/judge_utils.py
+++ lmms_eval/tasks/ayavisionbench/judge_utils.py
@@ -232,16 +232,21 @@ def run_judge(
     model = judge_config["judge_model_name"]
     
 
-    
-    if os.getenv("LITELLM_API_KEY"):
-        api_key = os.getenv("LITELLM_API_KEY")
-        # api_url = "https://cmu.litellm.ai"
-    elif os.getenv("ANTHROPIC_API_KEY"):
-        api_key = os.getenv("ANTHROPIC_API_KEY")
-        # api_url = "https://api.anthropic.com/v1/messages"
-    elif os.getenv("OPENAI_API_KEY"):
-        api_key = os.getenv("OPENAI_API_KEY")
-        # api_url = "https://api.openai.com/v1/chat/completions"
+    if judge_config["api_type"] == "anthropic":
+        if os.getenv("ANTHROPIC_API_KEY"):
+            api_key = os.getenv("ANTHROPIC_API_KEY")
+        else:
+            raise ValueError("No API key found. Please set ANTHROPIC_API_KEY environment variable or define the api_key in the judge_config")
+    elif judge_config["api_type"] == "openai":
+        if os.getenv("OPENAI_API_KEY"):
+            api_key = os.getenv("OPENAI_API_KEY")
+        else:
+            raise ValueError("No API key found. Please set OPENAI_API_KEY environment variable or define the api_key in the judge_config")
+    elif judge_config["api_type"] == "litellm":
+        if os.getenv("LITELLM_API_KEY"):
+            api_key = os.getenv("LITELLM_API_KEY")
+        else:
+            raise ValueError("No API key found. Please set LITELLM_API_KEY environment variable or define the api_key in the judge_config")
     else:
         raise ValueError("No API key found. Please set LITELLM_API_KEY, ANTHROPIC_API_KEY, or OPENAI_API_KEY environment variable or define the api_key in the judge_config")
 
@@ -287,7 +292,7 @@ def run_judge(
                     temperature=judge_config["temperature"] if "temperature" in judge_config else None,
                     top_p=judge_config["top_p"] if "top_p" in judge_config else None,
                     api_key=api_key,
-                    base_url=judge_config["api_url"] if "api_url" in judge_config else None
+                    base_url=judge_config.get("api_url")
                 )
                 responses.append(response)
                 
